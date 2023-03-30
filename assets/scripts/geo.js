064256fb5fdc968795aad7d1b871e257fe3637ed
TheAyes <Just.schildt@gmail.com>
2023-03-30 15:19:32 +0200

    Removed console.log from getGeoData

--- assets/scripts/geo.js
+++ assets/scripts/geo.js
@@ -3,7 +3,7 @@ const limit = "100";
 const getGeoData = async (city, stateCode, countryCode) => {
 	const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},${stateCode},${countryCode}&limit=${limit}&appid=${apiKey}`);
 	const data = await response.json().then(data => data[0]);
-	console.log(data);
+
 	return {
 		name: data.name,
 		state: data.state,
@@ -12,3 +12,4 @@ const getGeoData = async (city, stateCode, countryCode) => {
 		longitude: data.lon
 	}
 }
+
